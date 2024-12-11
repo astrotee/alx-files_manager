0d660c675426cab6556f2a8a3e47e8e9ebcab61e
amin ahmed <aminahmed24@gmail.com>
2024-12-11 14:36:52 +0200

    increase req.body size limit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import routes from './routes';
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 routes(app);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
